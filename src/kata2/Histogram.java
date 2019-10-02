066ed9f222b79a05b04fd36717a80f448967c984
Usuario <Usuario@10.22.148.100>
2019-10-02 09:31:42 +0100

    no hace uso del calculate al final

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -12,10 +12,6 @@ public class Histogram {
             counts.put(value,counts.getOrDefault(value, 0)+1);
     }
     
-    public void calculate(){
-        
-    }
-
     public Set<Integer> getValues() {
         return counts.keySet();
     }
