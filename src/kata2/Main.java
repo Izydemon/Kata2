7de71020ad84fd10a1a8510849cbfe3bdb80cf81
Usuario <Usuario@10.22.148.100>
2019-10-02 09:39:58 +0100

    main acabado

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -4,10 +4,9 @@ package kata2;
 public class Main {
 
     public static void main(String[] args) {
-        Histogram histogram = new Histogram(5,8,4,6,8,4,2,1,5,3,6,9,7);
-        for(int value : histogram.getValues()){
-            
-        } 
+        Histogram histogram = new Histogram(5,8,4,6,8,4,2,1,5,3,6,9,7,1,1,1,1);
+        HistogramDisplay display = new HistogramDisplay(histogram);
+        display.execute();
     }
     
 }
