a3cbb2fa0eb166f4dbbb3cb838ed5bd810059084
Usuario <Usuario@10.22.148.100>
2019-10-02 09:25:10 +0100

    Me he perdido

---
+++ src/kata2/Main.java
@@ -0,0 +1,13 @@
+
+package kata2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Histogram histogram = new Histogram(5,8,4,6,8,4,2,1,5,3,6,9,7);
+        for(int value : histogram.getValues()){
+            
+        } 
+    }
+    
+}
